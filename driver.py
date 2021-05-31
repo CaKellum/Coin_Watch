184e717495d3846a4f563093f6b770fdad29d155
CaKellum <christopherk379@gmail.com>
2021-05-31 11:50:45 -0400

    Refactor the driver operation

--- driver.py
+++ driver.py
@@ -1,19 +1,10 @@
 from .main import data_manager as dm
 from .main import figure_trend as ft
-from .main import manage_email as me
 
 
 def main():
-    result = dm.get_data()
-    if result == -1:
-        me.send_custom("coin_tracker now online")
-    else: 
-        desicion = ft.decide(result)
-        if(desicion['thought'] == 'sell'):
-            me.send_sell(desicion['price'])
-            dm.write_new(desicion)
-        else:
-            dm.write_new(desicion)
+    dm.update()
+    ft.decide()
 
 
 if __name__ == '__main__':
